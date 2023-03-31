072aa21084a6bdd463e702cf68293ee4acb91cdd
Maiiialen <magdalena.salega@gmail.com>
2023-03-31 16:55:52 +0200

    feat: animation of light mode changing

--- js/light-mode.js
+++ js/light-mode.js
@@ -1,8 +1,18 @@
 const page = document.getElementById("page")
+const circle = document.getElementById("circle")
 const modes = ["dark", "light"]
 
 function changeLightMode() {
-    page.className = modes[(modes.indexOf(page.className)+1)%2]
+    mode.removeEventListener("click", changeLightMode)
+    circle.className = modes[(modes.indexOf(page.className) + 1) % 2]
+    page.className = modes[(modes.indexOf(page.className) + 1) % 2]
+    circle.classList.add("animate")
+    setTimeout(restoreSize, 1500);
+}
+
+function restoreSize(){
+    circle.classList.remove("animate")
+    mode.addEventListener("click", changeLightMode)
 }
 
 mode.addEventListener("click", changeLightMode)
