2e5780205cff224b7c38bc0babc5f8e1a2f438f4
Maiiialen <magdalena.salega@gmail.com>
2023-03-31 15:40:02 +0200

    feat: add light mode changing

--- js/light-mode.js
+++ js/light-mode.js
@@ -1,8 +1,8 @@
-const mode = document.getElementById("mode")
 const page = document.getElementById("page")
+const modes = ["dark", "light"]
 
 function changeLightMode() {
-    // page.
+    page.className = modes[(modes.indexOf(page.className)+1)%2]
 }
 
 mode.addEventListener("click", changeLightMode)
