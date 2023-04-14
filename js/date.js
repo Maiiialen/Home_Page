9fca013ce9fe16ec392cec5e7b91dfb8f3e93d78
Maiiialen <magdalena.salega@gmail.com>
2023-04-14 16:57:37 +0200

    fix: fix date reading

--- js/date.js
+++ js/date.js
@@ -1,14 +1,26 @@
-window.onload = displayClock();
+let today = new Date()
 
-function setDate(dateObj, today, monthNames) {
+const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+]
+
+const dateObj = document.getElementById("date")
+const hourObj = document.getElementById("hour")
+const greeting = document.getElementById("greeting")
+
+function setDate() {
     dateObj.innerText = `${today.getDay() + 1} ${monthNames[today.getMonth()]} ${today.getFullYear()}`
+    return;
 }
 
-function setHour(hourObj, today) {
+function setHour() {
     hourObj.innerText = `${today.getHours()}:${('0' + today.getMinutes()).slice(-2)}`
+    // hourObj.innerText = `${today.getHours()}:${('0' + today.getMinutes()).slice(-2)}:${('0' + today.getSeconds()).slice(-2)}`
+    return;
 }
 
-function setGreeting(greeting, today) {
+function setGreeting() {
     const hour = today.getHours()
 
     if (hour >= 6 && hour < 12) {
@@ -18,23 +30,15 @@ function setGreeting(greeting, today) {
     } else {
         greeting.innerText = 'Good evening my Guest!';
     }
+    return;
 }
 
 function displayClock() {
-    const today = new Date()
-
-    const monthNames = [
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ]
-
-    const dateObj = document.getElementById("date")
-    const hourObj = document.getElementById("hour")
-    const greeting = document.getElementById("greeting")
-
-    setTimeout(() => {
-        setDate(dateObj, today, monthNames)
-        setHour(hourObj, today)
-        setGreeting(greeting, today)
-    }, 1000);
+    today = new Date()
+    setDate()
+    setHour()
+    setGreeting()
+    setTimeout(displayClock, 10);
 }
+
+window.onload = displayClock();
